04f468e12bf8c9929b455baaf601a2c047b41ca7
ISMAIL <ismailjosim99@gmail.com>
2023-07-07 00:35:44 +0600

    modify codde

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const server = jsonServer.create();
 const router = jsonServer.router('db_todos.json');
 const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 9000; // Use the provided environment port or 9000 as fallback
+
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${ PORT }`);
+});
