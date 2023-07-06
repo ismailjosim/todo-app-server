a4881f9dd13b1701d3d3db470e449e6f93d3037c
ISMAIL <ismailjosim99@gmail.com>
2023-07-07 00:46:23 +0600

    code changes

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db_todos.json');
+const router = jsonServer.router('db.json'); // Updated file name
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 9000; // Use the provided environment port or 9000 as fallback
+const PORT = process.env.PORT || 9000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${ PORT }`);
-});
+module.exports = server; // Export the server instance
+
+// This block is not necessary for Vercel deployment
+if (!process.env.NODE_ENV) {
+    server.listen(PORT, () => {
+        console.log(`JSON Server is running on port ${ PORT }`);
+    });
+}
